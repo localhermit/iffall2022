a754c0b933d87e34fde7220fa492055aa0d88a94
localhermit <97927489+localhermit@users.noreply.github.com>
2022-11-14 00:47:45 -0600

    Add files via upload

---
+++ microsite/initial-build/script.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//     $('.slider').change(function(){
+//         var slider=$(this).val();
+//         $('div.slide:nth-child(1)').css("margin-left",-slider*10+'%')
+//     })
+// })
